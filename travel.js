36a96c1674c429d8d5497d5d3cd7db7822a32c0d
Sherard <sherardg@pennmedicine.upenn.edu>
2019-09-28 09:54:37 -0400

    Updated folders and test travel data

--- travel.js
+++ travel.js
@@ -1,6 +1,6 @@
 // Dependencies
 var express = require("express");
-var mongojs = require("mongojs");
+var mongodb = require("mongodb");
 // Require axios and cheerio. This makes the scraping possible
 var axios = require("axios");
 var cheerio = require("cheerio");
@@ -8,14 +8,14 @@ var cheerio = require("cheerio");
 var app = express();
 
 // A GET route for scraping the echoJS website
-app.get("/scrape", function(req, res) {
+app.get("/travel", function(req, res) {
     // First, we grab the body of the html with axios
     axios.get("https://www.travel-advisory.info/api/").then(function(response) {
       // Then, we load that into cheerio and save it to $ for a shorthand selector
       var $ = cheerio.load(response.data);
   
       // Now, we grab every h2 within an article tag, and do the following:
-      $("iso_alpha").each(function(i, element) {
+      $("iso_alpha2").each(function(i, element) {
         // Save an empty result object
         var result = {};
   
